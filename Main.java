0b0449710a81e84f1ded756f5b1cb90814b05a3b
Tsubasa Sasaki <tsubasa.sasaki.sys21@morijyobi.ac.jp>
2022-03-22 11:38:02 +0900

    a

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+	public static void main (String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("身長(m)");
+		double sin=sc.nextInt();
+		System.out.println("体重(kg)");
+		double tai=sc.nextInt();
+		double BMI=tai/(sin*sin);
+		System.out.println("your BMI is"+BMI);
+		System.out.println(BMI);
+	}
+}
